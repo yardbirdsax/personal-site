5db0c915a649af95d64651cf477f3ee8ba89fb08
Josh Feierman <yardbirdsax@gmail.com>
2025-11-13 20:21:32 -0500

    Remove Gatsby files and dependencies
    This commit completes Task 8.6 of the migration plan by removing all Gatsby-specific files and directories from the Hugo migration worktree:
    
    - Removed Gatsby configuration files: package.json, package-lock.json, gatsby-config.js, gatsby-node.js, gatsby-browser.js, config.js
    - Removed src/ directory containing all React components, templates, and pages
    - Removed node_modules/ directory (Gatsby dependencies)
    - Removed .cache/ directory (Gatsby build cache)
    
    The Hugo site is now clean of all Gatsby dependencies and ready for final testing before merge to master.

--- src/components/panel/panelContainer.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import "./panel.css"
-
-const PanelContainer = ({children}) => (
-  <div className="panel-container">
-    {children}
-  </div>
-)
-export default PanelContainer
--- src/components/paypal-me/paypalmelink.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import "./paypalme.css"
-
-const PaypalMeLink = ({amount, text = "Purchase"}) => (
-    <a className="paypalme" href={`https://paypal.me/sqljosh/${amount}`}>{text}</a>
-)
-
-export default PaypalMeLink
